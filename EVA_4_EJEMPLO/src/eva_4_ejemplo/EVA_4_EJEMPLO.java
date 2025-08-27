0346afce463ecb46c0de597bf4cc71b84549c992
Alejandro128g <alejandrojacquez123@gmail.com>
2025-08-26 23:30:34 -0600

    Add files via upload

---
+++ EVA_4_EJEMPLO/src/eva_4_ejemplo/EVA_4_EJEMPLO.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_4_ejemplo;
+
+/**
+ *
+ * @author ALEJANDRO JAQUEZ
+ */
+public class EVA_4_EJEMPLO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("EMPLEO: ");
+        System.out.println("ING EN SITEMAS");
+        System.out.print("SALARI: ");
+        System.out.println(20000);
+        System.out.print("HORAS SEMANA: ");
+        System.out.println("6 ALA SEMANA");
+        System.out.print("REQUIERE INGLES? ");
+        System.out.println("SI");
+        
+       
+    }
+    
+}
